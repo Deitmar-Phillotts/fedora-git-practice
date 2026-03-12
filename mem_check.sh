747d8c88785b6523858c67fc56a36f375eec247c
Deitmar Phillpotts <dpphillpotts@my.waketech.edu>
2026-03-12 12:00:52 -0400

    Fixed awk syntax error by adding curly brackets to printf

--- mem_check.sh
+++ mem_check.sh
@@ -8,6 +8,6 @@ echo "-------------------------------" >> mem_report.txt
 # ps aux: list all processes
 # --sort--=-%mem: puts the highest memory users at the top
 # head -n 4: grabs the header + top 3
-ps aux --sort=-%mem | head -n 4 >> mem_report.txt
+ps aux --sort=-%mem | head -n 4 | awk '{printf "%-10s %-10s %-10s %-20s\n", $1, $2, $4, $11}' >> mem_report.txt
 
 cat mem_report.txt
